c03a51a6f2043521cce21e3cfe33fb531566c9f7
1999Snehal <snehallagad84@gmail.com>
2024-11-06 13:07:47 +0530

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+class MyAnotherThread extends Thread
+{
+    public void run()
+    {
+        for(int i = 10; i>=1; i--)
+        {
+            System.out.println("Value of i is:" +i);
+
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        MyAnotherThread t1 = new MyAnotherThread();
+        {
+            Thread thr = new Thread(t1);
+            thr.start();
+        }
     }
 }
